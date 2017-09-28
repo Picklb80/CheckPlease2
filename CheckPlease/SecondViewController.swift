4a19ccb95f48e71a780596b3a7dceb4b3a1555c5
Picklo,Blake H <s524406@ecusers-imac-16.nwmissouri.edu>
2017-09-28 11:26:06 -0500

    Initial Commit

---
+++ CheckPlease/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  CheckPlease
+//
+//  Created by Picklo,Blake H on 9/28/17.
+//  Copyright © 2017 Picklo,Blake H. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
